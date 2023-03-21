af74c2d8ca50f152fe368086a58a71c1b2b66763
SIMCON-VM\Creen <Milo Stankovi>
2023-03-21 16:47:45 +0100

    DTO fix

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -16,12 +16,16 @@ namespace API.Controllers
     }
 
     [HttpPost("register")]
-    public async Task<ActionResult<AppUser>> Register(string username, string password){
+    public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto)
+    {
+
+        if(await UserExists(registerDto.UserName)) return BadRequest("Username is taken");
+
         using var hmac = new HMACSHA512();
 
         var user = new AppUser{
-            UserName = username,
-            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(password)),
+            UserName = registerDto.Username.ToLower(),
+            PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(registerDto.Password)),
             PasswordSalt = hmac.Key
         };
 
@@ -29,6 +33,11 @@ namespace API.Controllers
         await _context.SaveChangesAsync();
 
         return user;
+    }
+
+    private async Task<bool> UserExists(string UserName)
+    {
+      return await _context.Users.AnySync(x => x.UserName == UserName.ToLower())
     }
   }
 }
