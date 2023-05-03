ce647b44942b5e212794962351e5e14e8706a186
SIMCON-VM\Creen <Milo Stankovi>
2023-05-03 23:28:41 +0200

    Member detail before fix

--- client/src/app/members/member-detail/member-detail.component.ts
+++ client/src/app/members/member-detail/member-detail.component.ts
@@ -24,6 +24,13 @@ export class MemberDetailComponent implements OnInit {
 
   ngOnInit(): void {
     this.loadMember();
+
+    this.route.queryParams.subscribe({
+      next: params => {
+        params['tab'] && this.selectTab(params['tab'])
+      }
+    })
+    
     this.galleryOptions = [
       {
         width: '500px',
